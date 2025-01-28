05ceceed60a1a74e69d87c5808a3afad6f448bb3
Mridul24agaga <mridulthareja8@gmail.com>
2025-01-29 00:32:03 +0530

    ASGAS

--- app/components/testimonialcard.tsx
+++ app/components/testimonialcard.tsx
@@ -35,7 +35,7 @@ export default function TestimonialsSection() {
       text: '"This is sooo good."',
       author: "Baback",
       role: "Founder",
-      avatar: "/cj.jpg",
+      avatar: "/cj.jpeg",
     },
     {
       text: '"The value for money is unmatched. For anyone serious about backlinks, this is a must-have tool"',
