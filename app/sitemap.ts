7bafd372aca4d6c534bd1a03a9fa757c007e3c0b
Mridul24agaga <mridulthareja8@gmail.com>
2025-01-29 15:46:21 +0530

    ASASG

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,86 +1,161 @@
-import { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next"
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.getmorebacklinks.org/',
-      lastModified: new Date('2024-12-11T17:51:14+00:00'),
+      url: "https://www.getmorebacklinks.org/",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
       priority: 1.0,
     },
     {
-      url: 'https://www.getmorebacklinks.org/blogs',
-      lastModified: new Date('2024-12-11T17:51:14+00:00'),
+      url: "https://www.getmorebacklinks.org/blogs",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
       priority: 0.8,
     },
     {
-      url: 'https://www.getmorebacklinks.org/submit',
-      lastModified: new Date('2024-12-11T17:51:14+00:00'),
+      url: "https://www.getmorebacklinks.org/auth-form",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
       priority: 0.8,
     },
     {
-      url: 'https://www.getmorebacklinks.org/privacy-policy',
-      lastModified: new Date('2024-12-11T17:51:14+00:00'),
+      url: "https://www.getmorebacklinks.org/landing-page-analyze",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
       priority: 0.8,
     },
     {
-      url: 'https://www.getmorebacklinks.org/terms',
-      lastModified: new Date('2024-12-11T17:51:14+00:00'),
+      url: "https://www.getmorebacklinks.org/seo-audit-website",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
       priority: 0.8,
     },
     {
-      url: 'https://www.getmorebacklinks.org/blogs/top-10-software-listing-websites-2025',
-      lastModified: new Date('2024-12-11T17:51:14+00:00'),
+      url: "https://www.getmorebacklinks.org/open-graph-validator",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.getmorebacklinks.org/meta-description-generator",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.getmorebacklinks.org/ai-content-checker",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.getmorebacklinks.org/paragraph-writer",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.getmorebacklinks.org/backlink-checker",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.getmorebacklinks.org/keyword-rank-tracker",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.getmorebacklinks.org/privacy-policy",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.getmorebacklinks.org/terms",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.getmorebacklinks.org/blogs/top-10-software-listing-websites-2025",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
       priority: 0.64,
     },
     {
-      url: 'https://www.getmorebacklinks.org/blogs/automatic-directory-submission-ai-revolution',
-      lastModified: new Date('2024-12-11T17:51:14+00:00'),
+      url: "https://www.getmorebacklinks.org/blogs/automatic-directory-submission-ai-revolution",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
       priority: 0.64,
     },
     {
-      url: 'https://www.getmorebacklinks.org/blogs/what-are-pbn-links-and-their-role-in-seo',
-      lastModified: new Date('2024-12-11T17:51:14+00:00'),
+      url: "https://www.getmorebacklinks.org/blogs/what-are-pbn-links-and-their-role-in-seo",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
       priority: 0.64,
     },
     {
-      url: 'https://www.getmorebacklinks.org/blogs/how-to-analyze-competitors-backlinks-for-seo-success',
-      lastModified: new Date('2024-12-11T17:51:14+00:00'),
+      url: "https://www.getmorebacklinks.org/blogs/how-to-analyze-competitors-backlinks-for-seo-success",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
       priority: 0.64,
     },
     {
-      url: 'https://www.getmorebacklinks.org/blogs/how-to-create-an-effective-seo-client-report-using-getmorebacklinks',
-      lastModified: new Date('2024-12-11T17:51:14+00:00'),
+      url: "https://www.getmorebacklinks.org/blogs/how-to-create-an-effective-seo-client-report-using-getmorebacklinks",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
       priority: 0.64,
     },
     {
-      url: 'https://www.getmorebacklinks.org/blogs/seo-reporting-for-agencies-how-getmorebacklinks-can-streamline-your-efforts',
-      lastModified: new Date('2024-12-11T17:51:14+00:00'),
+      url: "https://www.getmorebacklinks.org/blogs/seo-reporting-for-agencies-how-getmorebacklinks-can-streamline-your-efforts",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
       priority: 0.64,
     },
     {
-      url: 'https://www.getmorebacklinks.org/blogs/step-by-step-how-to-build-quality-backlinks-with-ai-powered-tools',
-      lastModified: new Date('2024-12-11T17:51:14+00:00'),
+      url: "https://www.getmorebacklinks.org/blogs/step-by-step-how-to-build-quality-backlinks-with-ai-powered-tools",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
       priority: 0.64,
     },
     {
-      url: 'https://www.getmorebacklinks.org/blogs/the-future-directory-submission-ai-and-automation-in-seo',
-      lastModified: new Date('2024-12-11T17:51:14+00:00'),
+      url: "https://www.getmorebacklinks.org/blogs/the-future-directory-submission-ai-and-automation-in-seo",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
       priority: 0.64,
     },
     {
-      url: 'https://www.getmorebacklinks.org/blogs/ultimate-guide-to-automated-directory-submission',
-      lastModified: new Date('2024-12-11T17:51:14+00:00'),
+      url: "https://www.getmorebacklinks.org/blogs/ultimate-guide-to-automated-directory-submission",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
       priority: 0.64,
     },
     {
-      url: 'https://www.getmorebacklinks.org/blogs/ultimate-guide-to-listing-bots',
-      lastModified: new Date('2024-12-11T17:51:14+00:00'),
+      url: "https://www.getmorebacklinks.org/blogs/ultimate-guide-to-listing-bots",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
       priority: 0.64,
     },
     {
-      url: 'https://www.getmorebacklinks.org/blogs/why-saas-founders-cant-ignore-directory-listings-in-their-marketing-strategy',
-      lastModified: new Date('2024-12-11T17:51:14+00:00'),
+      url: "https://www.getmorebacklinks.org/blogs/why-saas-founders-cant-ignore-directory-listings-in-their-marketing-strategy",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
       priority: 0.64,
+    },
+    {
+      url: "https://www.getmorebacklinks.org/blogs/the-smart-startup-guide-how-affilate-programs-and-seo-create-sustainable-saas-growth",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
+      priority: 0.64,
+    },
+    {
+      url: "https://www.getmorebacklinks.org/dashboard",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
+      priority: 0.64,
+    },
+    {
+      url: "https://www.getmorebacklinks.org/backlink-directory",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
+      priority: 0.64,
+    },
+    {
+      url: "https://www.getmorebacklinks.org/easylaunch",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
+      priority: 0.64,
+    },
+    {
+      url: "https://www.getmorebacklinks.org/hire-an-seo-expert",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
+      priority: 0.64,
+    },
+    {
+      url: "https://www.getmorebacklinks.org/notifications",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
+      priority: 0.64,
+    },
+    {
+      url: "https://www.getmorebacklinks.org/submit",
+      lastModified: new Date("2025-01-29T09:52:21+00:00"),
+      priority: 0.51,
     },
   ]
 }
